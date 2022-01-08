95aeeeb6616e014da65bf7adf4c847460111b215
Raj Dodiya <rdodiya2001@gmail.com>
2022-01-08 13:26:58 +0530

    Create project necessary files

---
+++ app/src/main/java/com/example/rentsell/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rentsell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
